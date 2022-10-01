02436aa59c217971f6b782d5bc15c7531473a997
Ho Ting Lau <114786597+lauhoting@users.noreply.github.com>
2022-09-30 22:57:56 -0400

    Add files via upload

--- SelectState.py
+++ SelectState.py
@@ -1,10 +1,19 @@
 import arcpy
 
-USStateMap = arcpy.GetParameterAsText(0) #r"E:\Previsico\WY\US_state\cb_2018_us_state_20m.shp"
-StateName = arcpy.GetParameterAsText(1) #"Wyoming"
-OutputFolder = arcpy.GetParameterAsText(2) #r"E:\Previsico\WY\WY_Boundary"
-StateAbbre = arcpy.GetParameterAsText(3) #"WY"
+#Directory of the US State Shapefile
+USStateMap = arcpy.GetParameterAsText(0)
 
+#State Full Name
+StateName = arcpy.GetParameterAsText(1)
+
+#Directory of Output Folder
+OutputFolder = arcpy.GetParameterAsText(2)
+
+#State Abbreviation
+StateAbbre = arcpy.GetParameterAsText(3)
+
+#Create Temporary Layer to Select State of Interest
 TemLay = arcpy.MakeFeatureLayer_management(USStateMap, StateAbbre, """ "NAME" = '{}' """.format(StateName))
 
+#Export the State of Interest as Shapefile
 arcpy.management.CopyFeatures(TemLay, OutputFolder+"\\{}.shp".format(StateAbbre))
